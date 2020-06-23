3ce539b7c1f43f49c65e52807ee87a06b6c0b6f7
SuperTapood <yoav.o252@gmail.com>
2020-06-23 22:00:49 +0300

    Update sanity_check.py

--- Tensorflow_2.0/sanity_check.py
+++ Tensorflow_2.0/sanity_check.py
@@ -1,9 +1,4 @@
 ## SANITY CHECK FOR THE ENTIRE FOLDER ##
-## TODO ##
-# numpy
-# pandas
-# matplotlib
-# six
 
 
 missing_modules = []
@@ -14,7 +9,22 @@ try:
 	assert int(tf.__version__[0]) == 2
 except:
 	missing_modules.append("Tensorflow>=2.0")
-
+try:
+	import six
+except:
+	missing_modules.append("six")
+try:
+	import matplotlib
+except:
+	missing_modules.append("matplotlib")
+try:
+	import pandas
+except:
+	missing_modules.append("pandas")
+try:
+	import numpy
+except:
+	missing_modules.append("numpy")
 
 ## output the missing modules, if any ##
 if missing_modules == []:
